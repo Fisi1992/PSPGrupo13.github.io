0b3c20dd2461f25e1ea1027f441b6ac25500debb
Fisi1992 <141073491+Fisi1992@users.noreply.github.com>
2024-05-27 22:23:05 -0500

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+document.getElementById('recipe-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Receta agregada con éxito!');
+});
+
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Formulario enviado con éxito!');
+});
